1d70e7a77393c0e1dc2f87dbf1fcf3f53af6b8f3
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-01 06:00:41 +0600

    learned three type of module

--- two-nodejs-pm-intro/index.js
+++ two-nodejs-pm-intro/index.js
@@ -1,6 +1,23 @@
+// local module
 const { add } = require("./other");
 
+// core modules
+const http = require('http');
 
-const result = add(2, 3);
+// third party module -> underscore
+const _= require('underscore');
 
-console.log(result);
+const server = http.createServer(function(req, res) {
+  // console.log(`server is running`);
+});
+server.listen(5000);
+
+// const result = add(2, 3);
+
+// console.log(result);
+
+var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+
+const res = _.pluck(stooges, 'age');
+
+console.log(res);
