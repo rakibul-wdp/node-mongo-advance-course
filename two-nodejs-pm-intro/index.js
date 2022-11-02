54a5baf5398a23bf31a9002013bb46440a0d32ff
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-02 06:56:41 +0600

    read txt file

--- two-nodejs-pm-intro/index.js
+++ two-nodejs-pm-intro/index.js
@@ -1,10 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   if (req.url == '/') {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<p>Welcome to Full Stack Development</p>');
     res.end();
+  } else if (req.url == '/read') {
+    fs.readFile('data.txt', (err, data) => {
+      if (err) {
+        res.write('Failed to read data...!!!');
+        res.end();
+      } else {
+        res.write(data);
+        res.end();
+      }
+    })
+  } else if (req.url == '/write') {
+
+  } else if (req.url == '/append') {
+
+  } else if (req.url == '/delete') {
+
   }
 });
 
