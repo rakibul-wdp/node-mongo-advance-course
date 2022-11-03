5561e0acd688f227e555ee6fa255517e2ba00b4e
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-03 07:17:24 +0600

    add descript by comment

--- three-express-rest-api/index.js
+++ three-express-rest-api/index.js
@@ -8,14 +8,14 @@ const port = process.env.PORT || 5000;
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 // const nodemailer = require("nodemailer");
 const dbConnect = require("./utils/dbConnect");
-const toolsRoutes = require("./routes/tools.route");
+const toolsRoutes = require("./routes/v1/tools.route");
 
 app.use(cors());
 app.use(express.json());
 
 dbConnect();
 
-app.use('/tools', toolsRoutes);
+app.use('/api/v1/tools', toolsRoutes);
 
 async function run() {
   try {
@@ -196,6 +196,10 @@ run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.all('*', (req, res) => {
+  res.send('No route found');
 });
 
 app.listen(port, () => {
